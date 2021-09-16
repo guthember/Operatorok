001b410cf227feb78b98a9b9044a79b25692e20f
jozsiegykecske <jozsiegykecske@gmail.com>
2021-09-16 11:44:56 +0200

    4.feladat elkészítése

--- Operatorok/Negyedik.cs
+++ Operatorok/Negyedik.cs
@@ -4,7 +4,28 @@
   {
     static public void NegyedikFeladat()
     {
-
+      System.Console.Write("4.feladat: ");
+      bool vane = false;
+      foreach (var sz in szamolasok)
+      {
+        if (sz.Op1 % 10 == 0 && sz.Op2 % 10 == 0)
+        {
+          vane = true;
+          
+        }
+        if (vane)
+        {
+          break;
+        }
+      }
+      if (vane)
+      {
+        System.Console.WriteLine("Van olyan kifejezés!");
+      }
+      else
+      {
+        System.Console.WriteLine("Nincs olyan kifejezés!");
+      }
     }
   }
 }
